35dbea20f028150d1a233981716d5e27f6522320
Artem <64410376+Artiss90@users.noreply.github.com>
2021-11-04 14:04:10 +0200

    add reverseWord

--- reverseWord.js
+++ reverseWord.js
@@ -1,5 +1,15 @@
 'use strict'
 
 function reverseWords(str) {
-    // Go for it
+  const words = str.split(' ')
+  const reverseWords = words.map( item => item.split('').reverse().join(''))
+  const phraseFromReverseWords = reverseWords.join(' ')
+
+  return phraseFromReverseWords
   }
+
+
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.'))// 'ehT kciuq nworb xof spmuj revo eht yzal .god'
+console.log(reverseWords('apple'))// 'elppa'
+console.log(reverseWords('a b c d'))// 'a b c d'
+console.log(reverseWords('double  spaced  words'))// 'elbuod  decaps  sdrow'  
