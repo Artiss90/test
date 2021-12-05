69f1da20aa8433038d149afe1882ecfca8a124b5
Artem <64410376+Artiss90@users.noreply.github.com>
2021-12-05 20:46:20 +0200

    Update moveZeros.js

--- moveZeros.js
+++ moveZeros.js
@@ -1,19 +1,15 @@
 'use strict'
 
-function twistedSum(n) {
-  let stringNumbers ="";
-  for (let prevNumber = n; prevNumber > 0; prevNumber -= 1){
-    stringNumbers += prevNumber
-  }
-  const numbers = stringNumbers.split('')
-  const sum = numbers.reduce((acc, value)=>{
-    acc += Number(value)
-    return acc
-  }, 0)
-  return sum;
+function moveZeros(arr) {
+  const withoutZero = [];
+  const zero = [];
+  arr.forEach( item => {
+    item === 0 ? zero.push(item) : withoutZero.push(item)
+  })
+  const result = withoutZero.concat(zero)
+  return result
 }
 
 
-console.log(twistedSum(4)) // 1 + 2 + 3 + 4 = 10
-console.log(twistedSum(10)) // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) = 46
-console.log(twistedSum(12)) // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) + (1 + 1) + (1 + 2) = 51
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"])) // returns[false,1,1,2,1,3,"a",0,0]
+
