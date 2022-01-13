28391033690514ae77fcf6e2bb5083a915f32a8c
Artem <64410376+Artiss90@users.noreply.github.com>
2022-01-13 11:54:43 +0200

    add binarySearch

---
+++ grocAlgorithm/binarySearchRecursively.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function binarySearchRecursively(arr, query) {
+    let startIndex = 0;
+    let endIndex = arr.length - 1;
+    while ( startIndex <= endIndex){
+        let mid = Math.floor((startIndex + endIndex) / 2);
+        const guess = arr[mid];
+        if(guess === query){
+            return guess
+        }
+        if(guess > query){
+            endIndex = mid - 1
+        }
+        if(guess < query){
+            startIndex = mid + 1
+        }
+    }
+    return null
+} 
+
+
+console.log(binarySearchRecursively([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 7));
+console.log(binarySearchRecursively([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 8));
+console.log(binarySearchRecursively([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 6));
+console.log(binarySearchRecursively([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 4));
+console.log(binarySearchRecursively([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], -1));
+// console.log(([1, 2, 3, 4, 5].find()));
