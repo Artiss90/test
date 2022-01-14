e68ece6352fe242e457a964fec72aae17dfdded5
Artem <64410376+Artiss90@users.noreply.github.com>
2022-01-14 07:58:41 +0200

    test

--- grocAlgorithm/recursivelyFactorial.js
+++ grocAlgorithm/recursivelyFactorial.js
@@ -5,9 +5,8 @@ const recursivelyFactorial = x => {
     return x * recursivelyFactorial(x - 1);
   };
   
-//   console.log(recursivelyFactorial(3));
-  console.log(recursivelyFactorial(3));
+
+  console.log(recursivelyFactorial(2));
   console.log(recursivelyFactorial(3));
   console.log(recursivelyFactorial(4));
   console.log(recursivelyFactorial(5));
-  console.log(recursivelyFactorial(6));
