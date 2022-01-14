7bbe7d8c6706022a647d7be479e0eb72945da5c8
Artem <64410376+Artiss90@users.noreply.github.com>
2022-01-14 07:22:49 +0200

    add recursivelyFactorial

---
+++ grocAlgorithm/recursivelyFactorial.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const recursivelyFactorial = x => {
+    if (x === 1) return 1;
+    return x * recursivelyFactorial(x - 1);
+  };
+  
+  console.log(recursivelyFactorial(4));
+  console.log(recursivelyFactorial(5));
+  console.log(recursivelyFactorial(6));
