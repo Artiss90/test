f1454d9f8e691d11351b989a7f4cb5386da6e468
Artem <64410376+Artiss90@users.noreply.github.com>
2022-06-06 21:07:56 +0300

    add other rowSumOddNumbers

--- algorithms/triangleHogben.js
+++ algorithms/triangleHogben.js
@@ -6,25 +6,31 @@ function rowSumOddNumbers(n) {
 
 // ?solution without constant triangle
 
-// function rowSumOddNumbers(n) {
-// //    The rows' start numbers are Hogben's centered polygonal numbers:
-//      1, 3, 7, 13, 21, 31, 43 = b[n] = n^2 - n + 1.
-//     //  <https://oeis.org/A002061>
+function rowSumOddNumbers(n) {
+  //    The rows' start numbers are Hogben's centered polygonal numbers:
+  //  1, 3, 7, 13, 21, 31, 43 = b[n] = n^2 - n + 1.
+  //  <https://oeis.org/A002061>
 
-//     //  The sum of one row is given by:
-//      s[n] = n^2 + n(b[n] - 1).
-//     //  <https://www.quora.com/What-is-the-sum-of-n-consecutive-odd-integers/answer/Xavier-Dectot>
+  //  The sum of one row is given by:
+  //  s[n] = n^2 + n(b[n] - 1).
+  //  <https://www.quora.com/What-is-the-sum-of-n-consecutive-odd-integers/answer/Xavier-Dectot>
 
-//     //  Inline b[n]:
-//      s[n] = n^2 + n(n^2 - n + 1 - 1)
-//           = n^2 + n(n^2 - n)
-//           = n^2 + n^3 - n^2
-//           = n^3
-//     //*  ... oh.
-//   return n * n * n;
-// }
+  //  Inline b[n]:
+  //  s[n] = n^2 + n(n^2 - n + 1 - 1)
+  //       = n^2 + n(n^2 - n)
+  //       = n^2 + n^3 - n^2
+  //       = n^3
+  //*  ... oh.
+  return n * n * n;
+}
 
-// Given the triangle of consecutive odd numbers:
+// ?or this solution
+function rowSumOddNumbers(n) {
+  const out = Array.from({ length: (n * (n + 1)) / 2 }, (_, i) => i * 2 + 1);
+  return out.slice(out.length - n).reduce((a, b) => a + b, 0);
+}
+
+// TODO Given the triangle of consecutive odd numbers:
 
 //              1
 //           3     5
